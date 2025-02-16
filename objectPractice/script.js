dd3c22b27170575cf73b42fd3b9571dea8c2cb64
Kirill Pashnin <kirillpashnin.dev@gmail.com>
2025-02-16 23:25:18 +0100

    Refactored so it uses class instead of plain constructors, and added styles

--- objectPractice/script.js
+++ objectPractice/script.js
@@ -1,16 +1,17 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-Book.prototype.toggleRead = function(index){
-  //Checks if the book is read
-  this.read = !this.read;
-  updateStatus(index);
-  showcaseLibrary();
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+  toggleRead(index) {
+    this.read = !this.read;
+    updateStatus(index);
+    showcaseLibrary()
+  }
 }
 
 function addBookToLibrary(title, author, pages, read) {
@@ -32,14 +33,13 @@ function showcaseLibrary() {
       <button class="read" data-index="${index}">${book.read ? 'Mark as Unread' : 'Mark as Read'}</button>
       <button id="removeBook" onclick="deleteBook(${index})">Delete book</button>
       `;//Creates a book DOM object, with the button having the index of the array object
-      
+
     container.appendChild(bookdiv);
     //Select read button
     const readBn = bookdiv.querySelector(".read");
-    readBn.addEventListener("click", () =>{
+    readBn.addEventListener("click", () => {
       book.toggleRead(index);
     })
-
   })
 }
 
@@ -48,11 +48,13 @@ const dialog = document.querySelector("dialog");
 const confirmBtn = document.getElementById("confirm");
 const addBookBtn = document.getElementById("addBookBtn");
 const cancelBtn = document.getElementById("cancel");
+const container = document.querySelector("container");
 addBookBtn.addEventListener("click", () => {
   dialog.showModal();
+  container.style.filter = "blur(100px)";
 })
 
- //Creates event listener for click, to retrieve data
+//Creates event listener for click, to retrieve data
 confirmBtn.addEventListener("click", () => {
   let bookTitle = document.getElementById("bookTitle").value;
   let bookAuthor = document.getElementById("bookAuthor").value;
@@ -67,7 +69,7 @@ cancelBtn.addEventListener("click", () => {
   dialog.close(); //Closes dialog window
 })
 
-function deleteBook(index){
+function deleteBook(index) {
   //Deletes object from array based on given index
   myLibrary.splice(index, 1)
   showcaseLibrary();
@@ -76,12 +78,14 @@ function deleteBook(index){
 function updateStatus(index) {
   const book = myLibrary[index]; // Get the book at the specified index
   const readButton = document.querySelector(`.read[data-index="${index}"]`); // Get the corresponding button
-  
+
   if (book.read) {
+    readButton.style.backgroundColor = "green"
     // If the book is read, set background to green
     readButton.textContent = "Mark as Unread"; // Update button text
   } else {
     // If the book is unread, set background to red
+    readButton.style.backgroundColor = "red"
     readButton.textContent = "Mark as Read"; // Update button text
   }
 }
